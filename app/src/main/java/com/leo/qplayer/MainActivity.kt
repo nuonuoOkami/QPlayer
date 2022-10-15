c5f74984bbf5a0e6897144d60aabeb5e17fe02be
Leo <1085214089@qq.com>
2022-10-15 20:42:25 +0800

    删除无用代码

--- app/src/main/java/com/leo/qplayer/MainActivity.kt
+++ app/src/main/java/com/leo/qplayer/MainActivity.kt
@@ -15,18 +15,13 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+
     }
 
-    /**
-     * A native method that is implemented by the 'qplayer' native library,
-     * which is packaged with this application.
-     */
+
     external fun stringFromJNI(): String
 
     companion object {
-        // Used to load the 'qplayer' library on application startup.
         init {
             System.loadLibrary("qplayer")
         }
