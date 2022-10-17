9d1ac5024c5a120a9d19d1a181bff56e65edf6be
诺诺大神 <1085214089@qq.com>
2022-10-17 18:51:00 +0800

    Player添加部分代码

--- app/src/main/cpp/QPlayer.h
+++ app/src/main/cpp/QPlayer.h
@@ -5,23 +5,26 @@
 #ifndef QPLAYER_QPLAYER_H
 #define QPLAYER_QPLAYER_H
 
+#include <pthread.h>
+
+extern "C"
+{
+#include <libavformat/avformat.h>
+#include <libavutil/time.h>
+};
 
 class QPlayer {
+private:
 
-    /**
-     * 准备
-     */
+    //路径
+    char *source = 0;
+    //准备线程
+    pthread_t p_thread_prepare;
+    //开始线程
+    pthread_t p_thread_start;
+    //媒体上下文
+    AVFormatContext *avFormatContext = 0;
 
-
-
-    /**
-     * 开始
-     */
-
-
-    /**
-     * 停止
-     */
 
 
 
